5104512b001c1a0961f6fe882aa6ac8e0cbfec77
Sid3Show <oli@live.co.uk>
2012-01-26 21:18:57 +0000

    Added hbs and improved routing

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 var express = require('express'),
     app = express.createServer(),
-    io = require('socket.io').listen(app);
+    io = require('socket.io').listen(app),
+    hbs = require('hbs');
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set("view engine", "hbs");
+  app.use(express.static(__dirname + "/public"));
+});
+
+
+
+hbs.registerHelper("css", function(url) {
+  return "<link rel='stylesheet' href='stylesheets/"+url+".css' type='text/css'>";
+});
+
+
+
+app.get('/', function (req, res) {
+
+  res.render("index", {});
+
+});
+
+var sockets = [];
+io.sockets.on('connection', function (socket) {
+  sockets.push(socket);
+  socket.on('message', function (data) {
+    for (var i=0;i<sockets.length;i++) {
+      sockets[i].emit('message', data);
+    }
+  });
+});
+
+app.listen(getPort());
+console.log("Server started on port "+getPort());
 
 function getPort() {
   var portFlag = process.argv.indexOf("-p");
@@ -11,19 +45,4 @@ function getPort() {
   }
 }
 
-app.use("/assets", express.static(__dirname + "/assets"))
-app.listen(getPort());
-console.log("Server started on port "+getPort());
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-var sockets = [];
-io.sockets.on('connection', function (socket) {
-  sockets.push(socket);
-  socket.on('message', function (data) {
-    for (var i=0;i<sockets.length;i++) {
-      sockets[i].emit('message', data);
-    }
-  });
-});
